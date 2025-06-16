9629b6defc801f074054e77eeb0756acd5bfb6d5
jooniku <joona.nikula@helsinki.fi>
2025-06-16 18:56:26 +0300

    Fix start script bug

--- start.sh
+++ start.sh
@@ -7,9 +7,6 @@ source venv/bin/activate
 # Install dependencies
 pip install flask
 
-# Navigate into your project
-cd tsoha || exit
-
 # Initialize DB, make upload folder, seed database, and run Flask
 flask init-db
 mkdir -p static/uploads
